7a35c825b174f703bcb37bb63c7fa7a9f033c836
qudotcom <ab.tahri2004@gmail.com>
2025-03-10 15:27:01 +0000

    Update temperature_converter.py

--- exercise3/temperature_converter.py
+++ exercise3/temperature_converter.py
@@ -17,7 +17,7 @@ def celsius_to_fahrenheit(celsius: Temperature) -> float:
     Returns:
         Temperature in Fahrenheit (rounded to 2 decimal places)
     """
-    return Celsius*(9/5) + 32
+    return round(Celsius*(9/5) + 32,2)
 
 
 def fahrenheit_to_celsius(fahrenheit: Temperature) -> float:
@@ -32,7 +32,7 @@ def fahrenheit_to_celsius(fahrenheit: Temperature) -> float:
     Returns:
         Temperature in Celsius (rounded to 2 decimal places)
     """
-    return (fahrenheit-32)*(5/9)
+    return round((fahrenheit-32)*(5/9),2)
 
 
 def celsius_to_kelvin(celsius: Temperature) -> float:
@@ -47,7 +47,7 @@ def celsius_to_kelvin(celsius: Temperature) -> float:
     Returns:
         Temperature in Kelvin (rounded to 2 decimal places)
     """
-    return celsius + 273.15
+    return round(celsius + 273.15, 2)
 
 
 def kelvin_to_celsius(kelvin: Temperature) -> float:
@@ -65,4 +65,4 @@ def kelvin_to_celsius(kelvin: Temperature) -> float:
     Raises:
         ValueError: If kelvin is less than 0 (below absolute zero)
     """
-    return kelvin - 273.15
+    return round(kelvin - 273.15, 2)
