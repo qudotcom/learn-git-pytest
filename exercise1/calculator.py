c34df30839d3ab352dbec84215f9b143a5c17e35
qudotcom <ab.tahri2004@gmail.com>
2025-03-10 14:38:21 +0000

    Update calculator.py

--- exercise1/calculator.py
+++ exercise1/calculator.py
@@ -16,8 +16,7 @@ def add(a: Number, b: Number) -> Number:
     Returns:
         The sum of a and b
     """
-    # TODO: Implement this function
-    pass
+   return a+b 
 
 
 def subtract(a: Number, b: Number) -> Number:
@@ -31,8 +30,7 @@ def subtract(a: Number, b: Number) -> Number:
     Returns:
         The result of a - b
     """
-    # TODO: Implement this function
-    pass
+    return a-b
 
 
 def multiply(a: Number, b: Number) -> Number:
@@ -46,8 +44,7 @@ def multiply(a: Number, b: Number) -> Number:
     Returns:
         The product of a and b
     """
-    # TODO: Implement this function
-    pass
+    return a*b
 
 
 def divide(a: Number, b: Number) -> Number:
@@ -64,5 +61,5 @@ def divide(a: Number, b: Number) -> Number:
     Raises:
         ValueError: If b is 0
     """
-    # TODO: Implement this function
-    pass
+    if b != 0 : return a/b
+    else : raise ValueError(" dividing by 0 is not supported ")
